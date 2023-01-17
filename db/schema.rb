9f7ff7e41298f00d4cb422deb32072fe5e2b14c0
Daylan Merritt <daylan.merritt@gmail.com>
2023-01-16 20:07:06 -0700

    order model, and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_12_173541) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_17_025545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_12_173541) do
     t.string "brand"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "order_quantity"
+    t.float "order_cost"
+    t.integer "order_number"
+    t.datetime "est_shipping"
+    t.string "address"
+    t.boolean "processed"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -56,4 +69,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_12_173541) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "orders", "users"
 end
