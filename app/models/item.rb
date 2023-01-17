f4e6763236a43aabda333dba99c7af8b3aa40b3c
Luis Arturo Topete Renteria <arturotopete31@gmail.com>
2023-01-16 20:24:13 -0700

    added through and has many but i left it commented out till ready to use

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
     validates :item_name, :description, :cost, :quantity, :category, :discount, :brand, presence: true
-
+    #has_many :users, through: :reviews
+    #has_many :reviews,  dependent: :destroy
 end
