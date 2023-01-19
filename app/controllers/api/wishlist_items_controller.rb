0ec49b1de079ce4ef17a91243271c8717b3b87ce
Luis Arturo Topete Renteria <arturotopete31@gmail.com>
2023-01-18 19:34:32 -0700

    model controller route #ed out

---
+++ app/controllers/api/wishlist_items_controller.rb
@@ -0,0 +1,46 @@
+class Api::WishlistItemsController < ApplicationController
+  before_action :set_wishlist
+  before_action :set_wishlist_item, only: [:show, :update, :destroy]
+  def index
+    render json: @wishlist.wishlist_items
+  end
+
+  def show
+    render json: @wishlist_item
+  end
+
+  def create
+    @wishlist_item = @wishlist.wishlist_items.new(wishlist_item_params)
+      if @wishlist_item.save
+        render json: @wishlist_item
+      else
+        render json: { errors: wishlist_item.errors }, status: :unprocessable_entity
+      end
+  end
+
+  def update
+    if @wishlist_item.update(wishlist_item_params)
+      render json:
+    else  
+      render json: {errors: @wishlist_item.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @wishlist_item.destroy
+    render json: { message: "bye bye wishlist_item"}
+  end
+
+  private 
+    def wishlist_item_params
+      params.require(:wishlist_item).permit(:wishlist_item_name, :desc, :plays_wishlist_item)
+    end
+    
+    def set_wishlist 
+      @wishlist = Wishlist.find(params[:wishlist_id])
+    end
+  
+    def set_wishlist_item
+      @wishlist_item= @wishlist.wishlist_items.find(params[:id])
+    end
+end
