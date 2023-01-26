613cfecbbb6f06805f4f828cbd92ebb74f318a8f
Luis Arturo Topete Renteria <arturotopete31@gmail.com>
2023-01-25 18:43:59 -0700

    bugfixing

--- client/src/components/items/CartButton.js
+++ client/src/components/items/CartButton.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+ import { useState, useEffect } from "react";
  import axios from "axios";
  import Dropdown from 'react-bootstrap/Dropdown';
  import DropdownButton from 'react-bootstrap/DropdownButton';
