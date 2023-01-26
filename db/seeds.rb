4dc5729854136c19c32557dd152bd009b5137154
hannah holt <hannahholt34@gmail.com>
2023-01-25 20:33:37 -0700

    filter

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,14 +13,16 @@ Item.delete_all
 end
 
 30.times do
+  brands = [ 'Disney', 'Lego', 'Barbie']
+  categories = ['Learning', 'Sports','Art']
   item = Item.create(
     item_name: Faker::FunnyName.name,
     description: Faker::Food.description,
     cost: Faker::Number.between(from: 0.0, to: 100),
     quantity: Faker::Number.between(from: 1, to: 10),
-    category: Faker::FunnyName.name,
+    category: categories.sample ,
     discount: Faker::Number.between(from: 0.0, to: 0.99),
-    brand: Faker::Food.fruits,
+    brand: brands.sample ,
     image: 'https://images.unsplash.com/photo-1582845512747-e42001c95638?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80'
   )
 end
