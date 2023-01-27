35af507a3409ea0d3f1cb2681c916bea7f6a4730
Daylan Merritt <daylan.merritt@gmail.com>
2023-01-26 18:06:45 -0700

    order functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,11 @@ Rails.application.routes.draw do
     resources :orders do
       resources :order_items
     end
-    resources :wishlists do
-      resources :wishlist_items
-    end
+    resources :wishlists
   end
+
+  resources :wishlists, except: [:index, :show, :create, :update, :destroy] do
+    resources :wishlist_items
+  end
+
 end
