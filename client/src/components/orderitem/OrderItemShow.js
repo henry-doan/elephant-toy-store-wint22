99f72db54dadcef03f4b0c537dca4a4ac83b04cc
Daylan Merritt <daylan.merritt@gmail.com>
2023-01-26 19:56:38 -0700

    stuff

--- client/src/components/orderitem/OrderItemShow.js
+++ client/src/components/orderitem/OrderItemShow.js
@@ -1,63 +1,3 @@
-<<<<<<< HEAD
-import { Card, Button, Modal, Container, Row, Col } from 'react-bootstrap';
- import { useState } from 'react';
- import { useParams } from 'react-router-dom';
- import { OrderConsumer } from '../../providers/OrderProvider';
-
-<<<<<<< HEAD
- const OrderShow = ({ id, deleteOrderItem}) => {
-   const [showing, setShow] = useState(false)
-=======
-const OrderShow = ({ id, deleteOrderItem}) => {
-  const [showing, setShow] = useState(false)
-  
-  const { orderId } = useParams()
-  // const { itemId } = useParams()
->>>>>>> 98466f3 (item linked to cart)
-
-   const { orderId } = useParams()
-   // const { itemId } = useParams()
-
-   return(
-     <>
-       <Card style={{ width: '15rem' }}>
-         <Card.Body>
-           <Button variant="outline-dark" onClick={() => setShow(true)}>
-             Show
-           </Button>
-         </Card.Body>
-       </Card>
-       <Modal show={showing} onHide={() => setShow(false)}>
-         <Modal.Header closeButton>
-         </Modal.Header>
-         <Modal.Body>
-           <Row>
-             <Col>
-               <Container>
-                 <Button
-                   onClick={() => deleteOrderItem(id)}
-                 >
-                   Delete
-                 </Button>
-               </Container>
-             </Col>
-           </Row>
-         </Modal.Body>
-       </Modal>
-       <br />
-     </>
-   )
- }
-
-
- const ConnectedOrderShow = (props) => (
-   <OrderConsumer>
-     { value => <OrderShow {...props} {...value} />}
-   </OrderConsumer>
- )
-
- export default ConnectedOrderShow;
-=======
 import ListGroup from 'react-bootstrap/ListGroup';
 import { OrderItemConsumer } from '../../providers/OrderItemProvider';
 import { useEffect, useState } from 'react';
@@ -66,7 +6,7 @@ import axios from 'axios';
 
 
 
-const OrderShow = ({ id, deleteOrderItem, orderId, addOrderitem, item_id}) => {
+const OrderShow = ({ id, deleteOrderItem, orderId, item_id}) => {
   const [item, setItem] = useState({ item_name: '' , description: '' , quantity: "" , category: "" , discount: "" , brand: "" , image: "" })
 
   useEffect(() => {
@@ -78,9 +18,8 @@ const OrderShow = ({ id, deleteOrderItem, orderId, addOrderitem, item_id}) => {
 
   return(
     <>
-{/* <a href={`/items/${item_id}`}></a> */}
       <ListGroup horizontal>
-        <ListGroup.Item><Image src={image} width='100px'/></ListGroup.Item>
+        <ListGroup.Item><a href={`/items/${item_id}`}><Image src={image} width='100px'/></a></ListGroup.Item>
         <ListGroup.Item>{item_name}</ListGroup.Item>
         <ListGroup.Item onClick={() => deleteOrderItem(id, orderId)}>-</ListGroup.Item>
       </ListGroup>
@@ -95,4 +34,3 @@ const ConnectedOrderItemShow = (props) => (
 )
 
 export default ConnectedOrderItemShow;
->>>>>>> 6918dbf (order functionality)
